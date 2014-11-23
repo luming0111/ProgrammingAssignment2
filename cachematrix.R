c3e9ea836385b2bd291f17491b5181b3f0265119
luming0111 <luming0111@gmail.com>
2014-11-23 22:16:40 +0800

    add cachematrix.R and try-run case.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,63 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Try run as below
+## x <- rbind(c(1, 5), c(5, 1))
+## cache <- makeCacheMatrix(x)
+## cache$get()
+##            [,1] [,2]
+##    [1,]    1    5
+##    [2,]    5    1
+## cacheSolve(cache)
+##        [,1]        [,2]
+##  [1,] -0.04166667  0.20833333
+##  [2,]  0.20833333 -0.04166667
 
+
+makeCacheMatrix <- function(x = matrix()) {
+  val <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    val <<- NULL
+  }
+  
+  get <- function() {
+    return(x)
+  }
+  
+  set_inverse <- function(solve){
+    val <<- solve
+  }
+  
+  get_inverse <- function(){
+    return(val)
+  }
+  
+  list(set = set, 
+       get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$get_inverse()
+  
+  if(!is.null(inverse)){    
+    return(inverse)
+  }
+  
+  data <- x$get()
+  
+  inverse <- solve(data)
+  
+  x$set_inverse(inverse)
+  
+  return(inverse)
 }
+
+
